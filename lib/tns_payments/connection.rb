4942f5556d659c2d8ad736ac3a6901f496576fab
Tim Cooper <coop@latrobest.com>
2012-03-14 10:45:49 +1000

    Reorder params.

--- lib/tns_payments/connection.rb
+++ lib/tns_payments/connection.rb
@@ -27,9 +27,9 @@ module TNSPayments
       transaction_id = transaction.transaction_id
       params         = {
         'apiOperation' => 'PAY',
-        'order'        => {'reference'      => transaction.reference},
         'cardDetails'  => card_details(token),
-        'transaction'  => {'amount'         => transaction.amount.to_s, 'currency' => transaction.currency, 'reference' => transaction_id.to_s}
+        'order'        => {'reference' => transaction.reference},
+        'transaction'  => {'amount'    => transaction.amount.to_s, 'currency' => transaction.currency, 'reference' => transaction_id.to_s}
       }
 
       request :put, "/merchant/#{merchant_id}/order/#{order_id}/transaction/#{transaction_id}", params
