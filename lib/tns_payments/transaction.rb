0b5cbfc92d6eacc8f19ac7f08b029ecb4138d3e6
Tim Cooper <coop@latrobest.com>
2012-04-03 14:55:55 +1000

    Allows minimum order_id to be injectable.

--- lib/tns_payments/transaction.rb
+++ lib/tns_payments/transaction.rb
@@ -1,12 +1,18 @@
 module TNSPayments
   class Transaction < SimpleDelegator
+    attr_writer :minimum_order_id
+
     def initialize transaction
       @transaction = transaction
       super
     end
 
+    def minimum_order_id
+      @minimum_order_id ||= 10000000000
+    end
+
     def order_id
-      10000000000 + @transaction.order_id.to_i
+      minimum_order_id + @transaction.order_id.to_i
     end
   end
 end
--- test/unit/transaction_test.rb
+++ test/unit/transaction_test.rb
@@ -1,11 +1,21 @@
 require 'helper'
 
 class TNSPayments::TransactionTest < MiniTest::Unit::TestCase
-  def test_defaults_order_id_to_tns_minimum
+  def test_order_id_is_tns_minimum
     transaction = MiniTest::Mock.new
     transaction.expect :order_id, 0
 
     assert_equal 10000000000, Transaction.new(transaction).order_id
     assert transaction.verify
+  end
+
+  def test_minimum_order_id_is_injectable
+    mock = MiniTest::Mock.new
+    mock.expect :order_id, 10
+    transaction = Transaction.new mock
+    transaction.minimum_order_id = 0
+
+    assert_equal 10, transaction.order_id
+    assert mock.verify
   end
 end
