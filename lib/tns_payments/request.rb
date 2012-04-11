3a4c7c3eaeed8e05c58e22c061aee48275a6760a
Tim Cooper <coop@latrobest.com>
2012-04-11 11:04:24 +1000

    Set a 120 second read timeout.

--- lib/tns_payments/request.rb
+++ lib/tns_payments/request.rb
@@ -3,23 +3,29 @@ require 'rest-client'
 
 module TNSPayments
   class Request
-    attr_accessor :connection, :method, :path, :options
+    attr_accessor :connection, :method, :path, :payload
 
-    def initialize connection, method, path, options = {}
-      self.connection, self.method, self.path, self.options = connection, method, path, options
+    def initialize connection, method, path, payload = {}
+      self.connection, self.method, self.path, self.payload = connection, method, path, payload
     end
 
     def perform
-      authorization = options.fetch(:authorization) { true }
+      authorization = payload.fetch(:authorization) { true }
       url           = "#{connection.host}/api/rest/version/4#{path}"
       auth_headers  = {:Authorization => encode_credentials}
       headers       = {:content_type => 'Application/json;charset=UTF-8', :accept => '*/*'}
       headers.merge! auth_headers if authorization
 
-      args = [headers]
-      args.unshift(options.to_json) unless [:delete, :get, :head].include? method
+      args = {
+        :method       => method,
+        :url          => url,
+        :payload      => payload.to_json,
+        :headers      => headers,
+        :timeout      => 120,
+        :open_timeout => 30
+      }
 
-      Response.new RestClient.send(method, url, *args)
+      Response.new RestClient::Request.execute(args)
     rescue RestClient::Exception => e
       Response.new({:result => e.message.upcase, :response => JSON.parse(e.response || '{}')}.to_json)
     end
